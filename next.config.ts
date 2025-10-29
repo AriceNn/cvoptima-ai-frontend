dffff34fe9bd74fb4baa57618ee8fd4790215cc3
AriceNn <ertuertu1000@gmail.com>
2025-10-29 22:54:55 +0300

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,19 @@
 // next.config.ts
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   devIndicators: false,
 
-  // --- YENİ KURAL BURADA ---
   async rewrites() {
     return [
       {
-        // Gelen kaynak path: /dl/ ile başlayan her şey (tek bir parametre alır)
-        source: '/dl/:path*', 
-        // Hedef: Backend API adresimiz
-        destination: 'https://cvoptima-api.onrender.com/dl/:path*', 
+        source: "/dl/:path*",
+        destination: "https://api.cvoptima.com/dl/:path*",  // <-- BURASI DÜZELDİ
       },
-    ]
+    ];
   },
-  // --- KURAL BİTTİ ---
 };
 
 export default nextConfig;
